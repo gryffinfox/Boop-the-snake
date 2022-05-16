fc6b38749fbbe88c627de8fcc26c3310ce0f6be4
Magdalena Morongova <magdalena.morongova@protonmail.com>
2022-05-16 10:08:04 +0200

    Snakes unified under Snake file, text -> static class, while loop negated

--- main.py
+++ main.py
@@ -1,78 +1,71 @@
 import random
 import sys
-import class_player
-import class_snake
-import class_text
-import class_boa
-import class_colubrid
-import class_viper
-
-all_snakes = []
+#import class_text -> file must be the same name as the class
+from text import Text
+from player import Player
+from snake import Snake
+from snake import european_snakes
 
 ################ GAME ###########################
 
-welcome()
-who_is_playing = input('What is your name? ')
-username = Player(who_is_playing)
-name_confirmation()
+Text.print_welcome()
+username= input('What is your name? ')
+player = Player(username)
+Text.confirm_username(player.name)
 
-trip = input('\nWhat do you choose?\nTrip to Europe (press 1) ')
-while trip != range(0-1):
+trip = input('\nWhere do you want to go?\nTrip to Europe (press 1) ')
+while trip not in ["1"]:
+    Text.wrong_input()
+    trip = input('\nWhere do you want to go?\nTrip to Europe (press 1) ')
     continue
 
 generated_snakes = []
 
 if trip == '1':
-    europe()
-    generated_snakes = random.choices(all_snakes, k = 5)
+    Text.print_europe()
+    generated_snakes = random.choices(european_snakes, k = 5)
     counter = 0
     for snake in generated_snakes:
         print('\nSnake n°', counter + 1, 'appears.')
 
-    while choose_action != range(0-5):
-        choose_action = input(choose_action())
-        continue
+        choose_action = input(f'{Text.choose_action()}')
+        while choose_action not in ['1', '2', '3', '4', '5', '6']:
+            Text.wrong_input()
+            choose_action = input(f'{Text.choose_action()}')
 
-    if choose_action == '1':
-        generated_snakes[counter].react(username)
-        print('Stats for', username.name,':', username.score, 'points', username.health * '♥')
-        break
+        if choose_action == '1':
+            generated_snakes[counter].react(player)
+            print('Stats for', player.name,':', player.score, 'points', player.health * '♥')
 
-    elif choose_action == '2':                    # Observe
-        print(generated_snakes[counter].looks)
-        print('While you were looking at the snake, he decided to leave and you lost a chance to boop it.')
-        break
+        elif choose_action == '2':                    # Observe
+            print(generated_snakes[counter].looks)
+            print('While you were looking at the snake, he decided to leave and you lost a chance to boop it.')
 
-    elif choose_action == '3':
-        random_index = random.randrange(len(generated_snakes[counter].codex_entry))
-        random_codex_entry = generated_snakes[counter].codex_entry[random_index]                                                                            # Codex
-        print(generated_snakes[counter].species + '\n' + random_codex_entry)
-        break
+        elif choose_action == '3':
+            random_index = random.randrange(len(generated_snakes[counter].codex_entry))
+            random_codex_entry = generated_snakes[counter].codex_entry[random_index]                                                                            # Codex
+            print(generated_snakes[counter].species + '\n' + random_codex_entry)
 
-    elif choose_action == '4':                    # Guess the name
-        print('4 - guessing the name')
-        break
+        elif choose_action == '4':                    # Guess the name
+            print('4 - guessing the name')
+            print(generated_snakes[counter].species)
 
-    elif choose_action == '5':                    # Leave the snake alone
-        print('Žížalka: No pain, no gain. While in a nature is ALWAYS smart to leave a snake alone, in this game you don\'t get anything.')
-        print('Stats for', username.name, ':', username.score, 'points', username.health, 'HP')
-        break
+        elif choose_action == '5':                    # Leave the snake alone
+            print('Žížalka: No pain, no gain. While in a nature is ALWAYS smart to leave a snake alone, in this game you don\'t get anything.')
+            print('Stats for', player.name, ':', player.score, 'points', player.health, 'HP')
 
-    elif choose_action == '6':
-        print('\nGAME OVER\n', username.name, 'gained', username.score, 'points.') # Exit the game
-        username.save_score()
-        sys.exit()
+        elif choose_action == '6':
+            print('\nGAME OVER\n', player.name, 'gained', player.score, 'points.') # Exit the game
+            player.save_score()
+            sys.exit()
 
-    counter += 1
+        counter += 1
 
-    if username.health <= 0 or counter >= 5:
-        print('\nGAME OVER\n' + username.name + ' gained', username.score, 'points.')
-        username.save_score()
-        #sys.exit()
+        if player.health <= 0 or counter >= 5:
+            print('\nGAME OVER\n' + player.name + ' gained', player.score, 'points.')
+            player.save_score()
+            #sys.exit()
 
     more = input('Do you want to try again? ')
     if more.lower in ['yes', 'yy', 'y', 'yeah']:
         print('Žížalka: Ok.')
-
-    else:
-        break
--- text.py
+++ text.py
@@ -1,22 +1,19 @@
+# Rename the class
+# Add all the strings
+# Rename the methods
+
 class Text:
-    def __init__(self):
-        self.name_confirmation =
-        self.choose_action = ('\nWhat do you want to do?\n'
-                                      'Boop the snake (press 1)\n'
-                                      'Observe the snake (press 2)\n'
-                                      'Search in the codex (press 3)\n'
-                                      'Guess (press 4)\n'
-                                      'Leave the snake alone (press 5)\n'
-                                      'Exit the game (press 6)\n ')
-    def welcome (self):
+    @staticmethod
+    def print_welcome():
         print('Welcome to the snake booping game!'
               'I am Žížalka and I will be your guide.')
 
-    def name_confirmation(self):
-        print('Žížalka:', who_is_playing, ','
-            'how pretty! Let\'s go for a trip.')
+    @staticmethod
+    def confirm_username(username):
+        print('Žížalka:', username, 'how pretty! Let\'s go for a trip.')
 
-    def choose_action(self):
+    @staticmethod
+    def choose_action():                        #Rename
         print('\nWhat do you want to do?\n'
                 'Boop the snake (press 1)\n'
                 'Observe the snake (press 2)\n'
@@ -25,16 +22,19 @@ class Text:
                 'Leave the snake alone (press 5)\n'
                 'Exit the game (press 6)\n')
 
-    def wrong_input(self):
+    @staticmethod
+    def wrong_input():
         print('\nŽížalka: Did the cat walk on your keyboard? To go for a trip, '
               'type the number of the trip and press enter.')
 
-    def europe(self):
+    @staticmethod
+    def print_europe():
         print('Žížalka: Welcome in Europe! '
             'You will encouter five snakes a with each you need to decide if you boop them or not.'
             '\nBooping will get you points but it can also cost you lives. Not booping will gain you nothing.'
             '\nGame is over if you run out of lives or if you finish encountering all snakes.')
 
-    def wrong_input_two(self):
+    @staticmethod
+    def wrong_input_two():
         print('\nŽížalka: I don\'t accept different answers than 1 (for boop), 2 (for observing), '
               '3 (for codex), 4 (for guessing) or 5 (to leave the game).')
