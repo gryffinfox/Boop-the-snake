db8c53a345575c3e7fc6f2fcfb0e9c884447baff
magdalena <magdalena.morongova@protonmail.com>
2022-09-26 20:26:27 +0200

    Hangman fix - the loop ends when all the letters are guessed

--- guess.py
+++ guess.py
@@ -1,8 +1,17 @@
 # The hangman game
+# To do:
+# 1. String announcements into one static class
+# 2. How to call it in the main.py, is play_game too long?
+# 3. Make it pretty
+# 4. ...
 
-# converting the secret word into a set, then we use the join() to create a string of unique letters
+# converting the secret word into a set, then we use the join() to create a string of unique letters; random order
 def get_unique_letters(word):
-    return ''.join(set(word))
+    unique_letters = set(word)
+    remove_this_elements = [' ', '-']
+    for element in remove_this_elements:
+        unique_letters.remove(element)
+    return ''.join(unique_letters)
 
 ##########################################
 def get_hangsnake_stage(remaining_attempts):
@@ -24,11 +33,11 @@ def print_secret_word(secret_word, guessed_letters, default_letters):
 
 #######################################
 
+# check if guessed letter is in the secret word
 def is_guess_in_secret_word(guess, secret_word):
-    #
     if len(guess) > 1: #or not guess.isalpha():
         print('Žížalka: Concentrate! Only single letters are allowed. Try again.')
-        guess = input('Guess a letter: ')
+        guess = input('Žížalka: Guess a letter: ')
     else:
         if guess in secret_word:
             return True
@@ -39,7 +48,8 @@ def is_guess_in_secret_word(guess, secret_word):
 
 def play_game(snake_name):
     print('Žížalka: Ready to play a game? This one works exactly like the hangman game. '
-          'Let\'s see if you can guess this word!\n')
+          'The secret word is made of snake\'s English name and Latin name'
+          'Let\'s see if you can guess it right!\n')
     remaining_attempts = 6
     default_letters = ' ' + '-'
     guessed_letters = ''
@@ -48,17 +58,17 @@ def play_game(snake_name):
     print_secret_word(secret_word, guessed_letters, default_letters)
 
     while remaining_attempts > 0 and len(guessed_letters) < len(get_unique_letters(secret_word)):
-        guess = input('Guess a letter: ')
+        guess = input('Žížalka: Guess a letter: ')
         guess_in_secret_word = is_guess_in_secret_word(guess, secret_word)
 
         if guess_in_secret_word:
             if guess in guessed_letters:
-                print('You have already guessed the letter {}'.format(guess))
+                print('Žížalka: You have already guessed the letter {}'.format(guess))
             else:
-                print('Yes! The letter {} is part of the secret word'.format(guess))
+                print('Žížalka: Yes! The letter {} is part of the secret word'.format(guess))
                 guessed_letters += guess
         else:
-            print('No! The letter {} is not part of the secret word'.format(guess))
+            print('Žížalka: No! The letter {} is not part of the secret word'.format(guess))
             remaining_attempts -= 1
 
         print(get_hangsnake_stage(remaining_attempts))
@@ -67,6 +77,6 @@ def play_game(snake_name):
         #print('\n\nNumber of letters guessed: {}\n'.format(len(guessed_letters)))
 
     if len(guessed_letters) == len(get_unique_letters(secret_word)):
-        print("+++ Well done, you have won this game! +++\n")
+        print("+++ Žížalka: Well done, young apprentice, you have won this game! +++\n")
     else:
-        print("--- Sorry, you have lost this game! ---\n")
+        print("--- Žížalka: Sorry mate, you have lost this game! Try it again. ---\n")
